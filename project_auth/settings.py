f92d51fccf5a552cbc9eeb6fd879a8ab76d1f3eb
AbdelfattahAlaaEldein <aa3914@fayoum.edu.eg>
2024-05-31 15:25:12 +0300

    core

--- project_auth/settings.py
+++ project_auth/settings.py
@@ -54,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'project_auth.urls'
 
